8fe2841edaa606d2d67a48107b54f7cf587a7129
Ken Schneider <ken.schneider@datadoghq.com>
2024-05-31 17:21:36 -0400

    increase windows buffer size

--- utils_windows.go
+++ utils_windows.go
@@ -3,17 +3,13 @@
 
 package probing
 
-import (
-	"golang.org/x/net/ipv4"
-	"golang.org/x/net/ipv6"
-)
-
 // Returns the length of an ICMP message, plus the IP packet header.
 func (p *Pinger) getMessageLength() int {
-	if p.ipv4 {
-		return p.Size + 8 + ipv4.HeaderLen
-	}
-	return p.Size + 8 + ipv6.HeaderLen
+	// if p.ipv4 {
+	// 	return p.Size + 8 + ipv4.HeaderLen
+	// }
+	// return p.Size + 8 + ipv6.HeaderLen
+	return 2048
 }
 
 // Attempts to match the ID of an ICMP packet.
